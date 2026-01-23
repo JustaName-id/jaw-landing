80b7d4bb39224d469118a198f18da99245019cd5
AngeloAyranji <angeloayranji@gmail.com>
2026-01-23 16:52:38 +0200

    chore: test sr-only

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,9 @@ import { TrustedPartnetIntegrations } from "@/components/";
 export default function Home() {
   return (
     <div className="flex pt-[10vh] flex-col bg-background w-full h-full">
+      <p className="sr-only">
+        Justalab landing page. Learn about our vision, explore the technology under the hood, discover core components, view trusted partners and integrations, and see our prototype in action.
+      </p>
       <Hero />
       <Vision />
       <UnderTheHood />
